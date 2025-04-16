5e579b7963445864da79d7ff51aa4211f3662229
Ryutera <kuya9823@gmail.com>
2025-04-16 23:23:49 +0100

    login & sign up page created

--- client/app/components/Timeline.tsx
+++ client/app/components/Timeline.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Post from './Post'
 
 const Timeline = () => {
   return (
@@ -18,7 +19,7 @@ const Timeline = () => {
         </button>
       </form>
     </div>
-    
+    <Post/>
   </main>
 </div>
   )
