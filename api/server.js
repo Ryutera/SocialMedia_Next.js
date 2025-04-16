b49fcbab6dc0a8699edcba22f686cd9d2a622178
Ryutera <kuya9823@gmail.com>
2025-04-16 18:03:29 +0100

    login api created

--- api/server.js
+++ api/server.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = 5000;
 const { PrismaClient } = require("../api/generated/prisma");
 const prisma = new PrismaClient();
-const bcryot = require("bcrypt")
-
-
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
 
 
 //jsonデータを送る
@@ -27,6 +27,33 @@ app.post("/api/auth/register", async (req, res) => {
     });
     return res.json({ user });
 });
+
+//jsonwebtoken（JWT）：ログイン後にトークンを発行する。
+// ユーザーログイン
+app.post("/api/auth/login", async(req,res)=>{
+    const {email, password
+    } = req.body
+    const user = await prisma.user.findUnique({where:{email}})
+
+    if (!user) {
+        return res.status(401).json({error:"email or password is wrong"})
+        
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordValid) {
+        return res.status(402).json({error:"wrong password"}
+
+        )
+        
+    }
+
+    const token = jwt.sign({id:user.id},process.env.SECRET_KEY ,{
+        expiresIn:"1d", 
+    })
+
+    return res.json({"token":token})
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
