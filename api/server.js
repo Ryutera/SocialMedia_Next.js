35f7cc864977a878a3d4873eaf2cf4a7ef5d4d9d
Ryutera <kuya9823@gmail.com>
2025-04-16 17:37:01 +0100

    api created

--- api/server.js
+++ api/server.js
@@ -1,27 +1,33 @@
-const express = require("express")
-const app = express()
-const PORT = 5000
-const {PrismaClient} = require("@prisma/client")
+const express = require("express");
+const app = express();
+const PORT = 5000;
+const { PrismaClient } = require("../api/generated/prisma");
+const prisma = new PrismaClient();
+const bcryot = require("bcrypt")
 
-const prisma = new PrismaClient()
 
 
+
+//jsonデータを送る
+app.use(express.json());
 
 
 // 新規ユーザー
-app.post("/api/auth/register",async (req,res)=>{
-const {username,email,password} = req.body
+app.post("/api/auth/register", async (req, res) => {
+    const { username, email, password } = req.body;
+
+    // パスワードをランダムで10回適当な文字に入れ替える
+    const hashedPassword = await bcryot.hash(password,10)
     const user = await prisma.user.create({
-        data:{
+        data: {
             username,
             email,
-            password,
+            password: hashedPassword,
         }
-    })
-    return res.json({user})
-})
+    });
+    return res.json({ user });
+});
 
-app.listen(PORT, ()=>{
-    console.log("serveri is running ")
-})
-
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
