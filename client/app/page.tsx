87a7e69f2439cf94f7655832b2a047946b8e350e
Ryutera <kuya9823@gmail.com>
2025-04-16 23:02:45 +0100

    submitform design created

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,12 @@
 import Head from "next/head";
 import Image from "next/image";
+import Timeline from "./components/Timeline";
 
 export default function Home() {
   return (
-    <>
-   
+    <div>
+   <Timeline/>
     
-    </>
+    </div>
   );
 }
